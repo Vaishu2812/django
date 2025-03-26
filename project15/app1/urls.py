990efde3fcbf288411e1a65a837fa00d0a6af4e9
Vaishali Ramesh <130435806+Vaishu2812@users.noreply.github.com>
2025-03-26 15:15:34 +0530

    Delete project15 directory

--- project15/app1/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from app1.views import *
-app_name='app1'
-
-urlpatterns=[
-    path('dad/',dad,name='dad'),
-    path('child1/',child1,name='child1'),
-
-]
--- project15/app2/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from app2.views import *
-app_name='app2'
-
-urlpatterns=[
-    path('mom/',mom,name='mom'),
-    path('cart/',cart,name='cart'),
-]
